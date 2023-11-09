ab17ca0170d541e3e6befb7720514393dc3b2b6a
Radhika-222 <wandareradhika@gmail.com>
2023-11-09 15:54:28 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment04_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare and initialize variables
+            string name ="Radhika";
+            int age = 23; // Replace with your actual age
+            double height = 175.5; // Replace with your actual height in centimeters
+
+            // Display the variables
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height (cm): " + height);
+
+            // Keep the console window open
+            Console.ReadLine();
+        }
+    }
+}
+    
+
